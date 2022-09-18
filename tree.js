b1fc7adcb3a919a9edcc6968711c8c9d762c385f
Brian Elizondo <brian_elizondo@msn.com>
2022-09-18 10:30:45 -0400

    Data Structures => Trees Exercises: Trees - sumValues, Given a n-ary tree of integers, return the sum of all the integers

--- tree.js
+++ tree.js
@@ -1,35 +1,44 @@
 /** TreeNode: node for a general tree. */
-
 class TreeNode {
-  constructor(val, children = []) {
-    this.val = val;
-    this.children = children;
-  }
+    constructor(val, children = []) {
+        this.val = val;
+        this.children = children;
+    }
 }
 
 class Tree {
-  constructor(root = null) {
-    this.root = root;
-  }
+    constructor(root = null) {
+        this.root = root;
+    }
 
-  /** sumValues(): add up all of the values in the tree. */
+    /** sumValues(): add up all of the values in the tree. */
+    sumValues(){
+        let sum = 0;
+        let toVisitStack = [this.root];
+        
+        if(toVisitStack[0] !== null){
+            while(toVisitStack.length){
+                let current = toVisitStack.pop();
+                
+                for(let child of current.children){
+                    toVisitStack.push(child);
+                }
+                sum += current.val;
+            }
+        }
+        return sum;
+    }
 
-  sumValues() {
-    
-  }
+    /** countEvens(): count all of the nodes in the tree with even values. */
+    countEvens() {
 
-  /** countEvens(): count all of the nodes in the tree with even values. */
+    }
 
-  countEvens() {
+    /** numGreater(lowerBound): return a count of the number of nodes
+    * whose value is greater than lowerBound. */
+    numGreater(lowerBound) {
 
-  }
-
-  /** numGreater(lowerBound): return a count of the number of nodes
-   * whose value is greater than lowerBound. */
-
-  numGreater(lowerBound) {
-
-  }
+    }
 }
 
 module.exports = { Tree, TreeNode };
